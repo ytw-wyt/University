8987affe6f2db0567cb8b3626d24b68dff98c2e3
Larry Heimann (Prof. H) <profh@cmu.edu>
2024-02-04 14:26:31 -0500

    Update department_test.rb

--- test/models/department_test.rb
+++ test/models/department_test.rb
@@ -2,6 +2,21 @@ require "test_helper"
 
 class DepartmentTest < ActiveSupport::TestCase
   # Matchers
+  should have_many(:faculties)
+  should have_many(:courses).through(:faculties)
+
+  should validate_presence_of(:name)
+  should validate_presence_of(:unit_prefix)
+  should validate_uniqueness_of(:unit_prefix)
+
+  should allow_value("01").for(:unit_prefix)
+  should allow_value("67").for(:unit_prefix)
+  should allow_value("99").for(:unit_prefix)
+
+  should_not allow_value("101").for(:unit_prefix)
+  should_not allow_value("7").for(:unit_prefix)
+  should_not allow_value("K9").for(:unit_prefix)
+  should_not allow_value(nil).for(:unit_prefix)
 
 
   # Context
